8aeb0d71cfdd45142d38a5a5315c0349c32f5265
vitalyakvas <vitalyakvas@gmail.com>
2015-08-09 17:19:40 +0300

    Add base class for view model.

---
+++ WPFCustomTemplate/ViewModels/BaseViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+
+namespace WPFCustomTemplate.ViewModels
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        #region Implementation of INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs args)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, args);
+        }
+
+        #endregion
+    }
+}
