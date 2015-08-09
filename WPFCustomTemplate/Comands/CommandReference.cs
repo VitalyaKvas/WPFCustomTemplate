9813430e959ae5fa6dcfcfc02f50d87a2d5c4bf5
vitalyakvas <vitalyakvas@gmail.com>
2015-08-09 17:17:24 +0300

    Add class Commands for UI.

---
+++ WPFCustomTemplate/Comands/CommandReference.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WPFCustomTemplate.Comands
+{
+    public class CommandReference : Freezable, ICommand
+    {
+        public static readonly DependencyProperty CommandProperty;
+
+        static CommandReference()
+        {
+            CommandProperty = DependencyProperty.Register("Command", typeof(ICommand), typeof(CommandReference), new PropertyMetadata());
+        }
+
+        public CommandReference() { }
+
+        public ICommand Command
+        {
+            get { return (ICommand)GetValue(CommandProperty); }
+            set { SetValue(CommandProperty, value); }
+        }
+
+        #region ICommand Members
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        bool ICommand.CanExecute(object parameter)
+        {
+            if (Command != null)
+                return Command.CanExecute(parameter);
+            return false;
+        }
+
+        void ICommand.Execute(object parameter)
+        {
+            Command.Execute(parameter);
+        }
+
+        #endregion
+
+        #region Freezable
+
+        protected override Freezable CreateInstanceCore()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ WPFCustomTemplate/Comands/DelegateCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WPFCustomTemplate.Comands
+{
+    public class DelegateCommand : ICommand
+    {
+        private readonly Action executeMethod = null;
+
+        public DelegateCommand(Action executeMethod)
+        {
+            if (executeMethod == null)
+                throw new ArgumentNullException("executeMethod");
+
+            this.executeMethod = executeMethod;
+        }
+
+        #region ICommand Members
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        bool ICommand.CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        void ICommand.Execute(object parameter)
+        {
+            if (executeMethod != null)
+                executeMethod();
+        }
+        #endregion
+    }
+}
